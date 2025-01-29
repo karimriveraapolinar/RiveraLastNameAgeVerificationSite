70157649e4205c744b44969f85076e35926771d8
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2025-01-29 12:47:35 -0500

    fixed

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let age;
 mySubmit.onclick = function() {
     age = myText.value;
     age = Number(age);
-if ( age == 1 && age < 18){
-    myResult.textContent = `You are ${age}, which is bettween the ages of 1 and 18, dont stay up to late you child.`
+if ( age >= 1 && age < 18){
+    myResult.textContent = `You are ${age}, which is between the ages of 1 and 18, dont stay up to late you child.`
 } else if (age >= 18 && age <= 65){
     myResult.textContent = `You are ${age}, which is between the ages of 18 and 65, labeling you as an adult`
 } else if (age > 65 && age <= 100){
