2a08175573b62bad480228c005904cf432fece84
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2025-01-29 12:32:22 -0500

    added a little more

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ let age;
 mySubmit.onclick = function() {
     age = myText.value;
     age = Number(age);
-
-    if (age < 18) {
-        myResult.textContent = 'You are under 18.';
-    } else if (age >= 18 && age < 65) {
-        myResult.textContent = 'You are an adult.';
-    } else {
-        myResult.textContent = 'You are a senior.';
-    }
-};
+if ( age == 1 && age < 18){
+    myResult.textContent = `You are ${age}, which is bettween the ages of 1 and 18, dont stay up to late you child.`
+} else if (age >= 18 && age <= 65){
+    myResult.textContent = `You are ${age}, which is between the ages of 18 and 65, labeling you as an adult`
+} else if (age > 65 && age <= 100){
+    myResult.textContent = `You are ${age}, which is between the ages of 65 and 100, thanks grandma/pa.`
+} else if (age > 100){
+    myResult.textContent = `You are ${age}, you're pretty much a fossil at this point.`
+} else{
+    myResult.textContent = `You are ${age}, which means you shouldn't be alive.`
+}
+}
